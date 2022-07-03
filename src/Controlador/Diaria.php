b250f81fdc218d7c546e8276f542545a58d1e6f4
Alexander Fonseca <motoleco@hotmail.com>
2022-07-02 21:14:44 -0300

    criando projeto lista de diárias

---
+++ src/Controlador/Diaria.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\controlador;
+//vai obter os dados através do modelo e chamar camada visualização
+class Diaria
+{
+  public function listar()
+  {
+    # code...
+  }
+}
--- src/Controlador/Diarista.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\controlador;
-
-class Diarista
-{
-  public $nome;
-}
